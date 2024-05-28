84bfa64492c19e4acead9a9cb79068837c38e0d1
siddharthmittal97 <siddharthmittal97@gmail.com>
2024-05-28 21:23:08 +0530

    Create  Find me the first and last occurrence of the letter ‘a’ in amazonaanazzz with index number.java

---
+++ Find me the first and last occurrence of the letter ‘a’ in amazonaanazzz with index number.java
@@ -0,0 +1,9 @@
+class HelloWorld 
+{
+    public static void main(String[] args) 
+    {
+        String s="amazonaanazzz";
+        System.out.println(s.indexOf('a'));
+        System.out.println(s.lastIndexOf('a'));
+    }
+}
