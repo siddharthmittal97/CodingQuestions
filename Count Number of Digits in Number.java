41a11d1f02a6b18aed04a26cf57ff6924c32dc9e
siddharthmittal97 <siddharthmittal97@gmail.com>
2024-06-03 00:26:32 +0530

    Create Count Number of Digits in Number.java

---
+++ Count Number of Digits in Number.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a");
+        int n=sc.nextInt();
+        int count=0;
+        while(n>0)
+        {
+            n=n%10;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
