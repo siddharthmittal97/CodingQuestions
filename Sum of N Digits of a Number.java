542dc61fdcfe20c97ad853b0cd2aa4910fad4e7e
siddharthmittal97 <siddharthmittal97@gmail.com>
2024-06-03 00:48:18 +0530

    Create Sum of N Digits of a Number.java

---
+++ Sum of N Digits of a Number.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter n");
+        int n=sc.nextInt();
+        int sum=0;
+        while(n>0)
+        {
+            int rem=n%10;
+            sum=sum+rem;
+            n=n/10;
+        }
+        System.out.println(sum);
+    }
+}
